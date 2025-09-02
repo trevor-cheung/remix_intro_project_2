825edf8ad8001c5fc0fa38174988cca721d03225
trevor-cheung <t6cheung@uwaterloo.ca>
2025-09-01 20:48:16 -0400

    fixed "cannot find name process" issue with drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,9 +6,9 @@ export default {
   schema: "./database/schema.ts",
   dialect: "sqlite",
   driver: "d1-http",
-  // dbCredentials: {
-  //   databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-  //   accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-  //   token: process.env.CLOUDFLARE_TOKEN!,
-  // },
+  dbCredentials: {
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    token: process.env.CLOUDFLARE_TOKEN!,
+  },
 } satisfies Config;
